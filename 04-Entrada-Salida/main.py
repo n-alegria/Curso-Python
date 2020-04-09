8dda5f73d9c31ff72bb4ef4ca6e1d35770d4ef10
Nestor <n.alegria@outlook.com>
2020-04-09 04:10:25 -0300

    Entrada y salida de datos

--- 04-Entrada-Salida/main.py
+++ 04-Entrada-Salida/main.py
@@ -6,7 +6,24 @@ edad = input("¿Cual es tu edad?: ") # El dato ingresado es un string
 numero = int(input("¿Cual es tu numero?: ")) # Casteo
 
 
-# Salida
+print("*** SALIDA ***\n")
 print(f"Hola {nombre}, tenes {edad}")
 print(type(edad))
 print(type(numero))
+
+
+# Concatenacion
+# Unir dos variables (string) -> se utliza el signo + para concatenar dos string
+nombre = "Nestor"
+apellido = "Alegria"
+hobby = "Programador"
+
+print(nombre + " " + apellido + " - " + hobby)
+
+# Con la f"{}" puedo formatear directamente la varaible en el print -> debo utilizar {} y dentro el nombre de las varaibles
+print(f"{nombre} {apellido} - {hobby}")
+
+# Se puede concatenar de otra forma.
+# Se utiliza la funcion ".format()", se debe usar { } donde quiero aplicar una variable y luego en el .format() escribo las variables en el orden de aparicion
+print("Hola me llamo {} {} y mi hobby es {}".format(nombre, apellido, hobby))
+print("Hola me llamo {variable1} {variable2} y mi hobby es {variable3}".format(variable1=nombre, variable2=apellido, variable3=hobby))
