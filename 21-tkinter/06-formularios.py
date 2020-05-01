d347efa58909b803c46eabe525cb0f536a1e155e
Lautaro <n.alegria@outlook.com>
2020-05-01 05:15:05 -0300

    Tkinter

--- 21-tkinter/06-formularios.py
+++ 21-tkinter/06-formularios.py
@@ -13,7 +13,41 @@ encabezado.config(
     padx = 10,
     pady = 10
 )
-encabezado.pack(side = LEFT, anchor = NW, fill = X, expand = YES)
+encabezado.grid(row = 0, column = 0, columnspan = 20, sticky = W) # sticky = pegar al lado
+
+# Label para el campo - Nombre
+label = Label(ventana, text = "Nombre")
+label.grid(row = 1, column = 0, padx = 5, pady = 5, sticky = W)
+
+# Campo de texto - ingreso de informacion
+# En lugar de usar '.pack()' puedo usar '.grid(fila, clumnda, padx, pady)'
+campo_texto = Entry(ventana)
+campo_texto.grid(row = 1, column = 1, padx = 5, pady = 5, sticky = W)
+campo_texto.config(justify = "right", state = "normal") # state -> DISABLES (lo deshabilito)
+
+# Label para el campo - Apellido
+label = Label(ventana, text = "Apellido")
+label.grid(row = 2, column = 0, padx = 5, pady = 5, sticky = W)
+campo_texto = Entry(ventana)
+campo_texto.grid(row = 2, column = 1, padx = 5, pady = 5, sticky = W)
+campo_texto.config(justify = "left", state = "normal") # justify -> me indica de que lado estará justificado el texto
+
+# Label para el campo - Descripcion
+label = Label(ventana, text = "Descripcion")
+label.grid(row = 3, column = 0, padx = 5, pady = 5, sticky = NW)
+
+
+# Campo de texto grande para la descripcion
+campo_grande = Text(ventana)
+campo_grande.grid(row = 3, column = 1)
+campo_grande.config(
+    width = 30,
+    height = 5,
+    font = ("Arial", 12),
+    padx = 15,
+    pady = 15
+)
+
 
 
 ventana.mainloop()
